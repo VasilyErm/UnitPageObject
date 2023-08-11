5c031560db9bc8a9ebb3372d33449b8b4c19339a
Vasily Ermolaev <vasily.erm@gmail.com>
2023-08-11 15:06:46 +0300

    Add CI

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,14 +1,22 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private SelenideElement header = $("[data-test-id=dashboard]");
 
     public DashboardPage(){
         header.should(visible);
+    }
+    private ElementsCollection cards = $$(".list__item");
+    public int getFirstCardBalance() {
+        val text = cards.first().text();   //text: "**** **** **** 0001, баланс: 10000 р. \nПополнить
+        return 0;
     }
 }
